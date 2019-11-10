95681273efc8cfe7744af099315d92156a4c6df9
swornimbarahi <sbara002@plattsburgh.edu>
2019-11-10 14:37:06 -0500

    Added collection for admins and remove function

--- functions/src/adminModeration.ts
+++ functions/src/adminModeration.ts
@@ -1,20 +1,55 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import {
+	Firestore,
+	QuerySnapshot,
+	QueryDocumentSnapshot
+} from "@google-cloud/firestore";
 
-admin.initializeApp({});
+const firestore: Firestore = admin.firestore();
 
-export const makeAdmin = functions.https.onCall((data, context) => {
-  return admin.auth().getUserByEmail(data.email).then((user: { uid: any; }) => {
-    return admin.auth().setCustomUserClaims(user.uid, {
-      admin: true
-    });
-  }).then(() => {
-    return {
-      message: `Success! ${data.email} was made an admin.`
-    }
-  }).catch((err: Error) => {
-    console.error(err);
-    return err;
-  });
-}
-)
+export const makeAdmin = functions.https.onCall(async (data, context) => {
+	try {
+		const user = await admin.auth().getUserByEmail(data.email);
+		await admin.auth().setCustomUserClaims(user.uid, {
+			admin: true
+		});
+
+		await firestore
+			.collection("admins")
+			.doc(data.email)
+			.set({
+				data: data.email
+			});
+
+		return {
+			message: `Success! ${data.email} was made an admin.`
+		};
+	} catch (err) {
+		return {
+			message: `Failure! ${data.email} could not be made an admin.`
+		};
+	}
+});
+
+export const removeAdmin = functions.https.onCall(async (data, context) => {
+	try {
+		const user = await admin.auth().getUserByEmail(data.email);
+		await admin.auth().setCustomUserClaims(user.uid, {
+			admin: false
+		});
+
+		await firestore
+			.collection("admins")
+			.doc(data.email)
+			.delete();
+
+		return {
+			message: `Success! ${data.email} was made an admin.`
+		};
+	} catch (err) {
+		return {
+			message: `Failure! ${data.email} could not be made an admin.`
+		};
+	}
+});
